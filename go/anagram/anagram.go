971beecbad2f73dd6e139b6c509c9abb430b794a
Costi <comisarulmoldovan@gmail.com>
2018-10-25 22:28:19 -0500

    Anagram scaffolding

---
+++ go/anagram/anagram.go
@@ -0,0 +1,7 @@
+package anagram
+
+// Detect returns the list of words that are anagrams from the candidates array
+func Detect(subject string, candidates []string) []string {
+	value := []string{"LOL"}
+	return value
+}
